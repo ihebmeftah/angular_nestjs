bb9d6dda0906bd656a1310db8de875f169142158
ihebmeftah <ihebmeftah666@gmail.com>
2025-03-02 16:33:39 +0100

    Add Docker Compose configuration and configure TypeORM for PostgreSQL

--- rh_server/src/app.module.ts
+++ rh_server/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admindb',
+      password: 'admindb',
+      database: 'rh_managment',
+      // entities: ['dist/**/*.entity{.js,.ts}'],
+      // migrations: ['dist/db/migrations/*{.js,.ts}'],
+      synchronize: false,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
