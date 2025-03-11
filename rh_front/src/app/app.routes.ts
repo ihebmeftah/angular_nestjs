84845227b25d04773a7dfdf056ba3b671954683c
ihebmeftah <ihebmeftah666@gmail.com>
2025-03-11 21:56:24 +0100

    Add authentication component with routing and styles

--- rh_front/src/app/app.routes.ts
+++ rh_front/src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
