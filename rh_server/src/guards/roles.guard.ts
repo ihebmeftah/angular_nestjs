4b11de44d98dac87fc9ecaa43e09e5b80fa507fc
ihebmeftah <ihebmeftah666@gmail.com>
2025-03-21 13:54:03 +0100

    Add roles decorator and guard for role-based access control

---
+++ rh_server/src/guards/roles.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { User } from 'src/user/entities/user.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) { }
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.get<string[]>(ROLES_KEY, context.getHandler());
+    if (!requiredRoles) return true;
+
+    const request = context.switchToHttp().getRequest();
+    const user: User = request.user;
+
+    return requiredRoles.includes(user.role);
+  }
+}
