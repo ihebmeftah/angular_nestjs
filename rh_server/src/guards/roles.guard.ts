5b3db9ea32ee95d12767b4f9a99f0e87112be609
ihebmeftah <ihebmeftah666@gmail.com>
2025-05-04 13:43:30 +0100

    Enhance RolesGuard to include ForbiddenException for unauthorized access

--- rh_server/src/guards/roles.guard.ts
+++ rh_server/src/guards/roles.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/roles.decorator';
 import { User } from 'src/user/entities/user.entity';
@@ -10,10 +10,15 @@ export class RolesGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.get<string[]>(ROLES_KEY, context.getHandler());
     if (!requiredRoles) return true;
-
     const request = context.switchToHttp().getRequest();
     const user: User = request.user;
-
-    return requiredRoles.includes(user.role);
+    console.log('User:', user);
+    console.log('Required Roles:', requiredRoles);
+    const isAuthorized = requiredRoles.includes(user.role);
+    if (!isAuthorized) {
+      throw new ForbiddenException(`Only ${requiredRoles} are authorized to access this route, current role is ${user.role}`);
+    } else {
+      return true;
+    }
   }
 }
