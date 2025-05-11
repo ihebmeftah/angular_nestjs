e36d9481b951acc35f7249171be10ab2b66359a1
ihebmeftah <ihebmeftah666@gmail.com>
2025-05-11 11:11:14 +0100

    Add NotFound component with routing and styling

--- rh_front/src/app/app.routes.ts
+++ rh_front/src/app/app.routes.ts
@@ -1,16 +1,23 @@
 import { Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
   {
     path: 'auth',
     children: [
-      { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent) },
-      { path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent) },
+      { path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+      },
+      { path: 'register',
+        loadComponent: () => import('./auth/register/register.component').then(m => m.RegisterComponent)
+      },
     ]
   },
   {
     path: 'home',
     loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)
   },
+  { path: 'not-found', component: NotfoundComponent }, // Wild route
+  { path: '**', redirectTo: 'not-found' }, // Wild route
 ];
 
