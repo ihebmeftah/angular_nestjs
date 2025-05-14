dbe08c24fb11d18939ab46a2cec88f2ac967f1ad
ihebmeftah <ihebmeftah666@gmail.com>
2025-05-14 19:11:00 +0100

    feat: Implement empty and loading states with enhanced styling and functionality

--- rh_front/src/app/common/empty/empty.component.ts
+++ rh_front/src/app/common/empty/empty.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-empty',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './empty.component.html',
   styleUrl: './empty.component.css'
 })
 export class EmptyComponent {
-
+  @Input() title?: string;
+  @Input() description?: string;
+  @Input() actionText?: string;
+  @Input() actionIcon?: string;
+  @Input() actionRoute?: string;
+  @Output() action = new EventEmitter<void>();
 }
--- rh_front/src/app/common/loading/loading.component.ts
+++ rh_front/src/app/common/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './loading.component.css'
 })
 export class LoadingComponent {
-
+  @Input() message?: string;
+  @Input() progress?: number;
 }
